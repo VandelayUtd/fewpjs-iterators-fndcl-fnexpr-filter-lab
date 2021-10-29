31fda013e756f23b410ab8ca1ddde34a1ca77665
VandelayUtd <drewhadley.guitar@gmail.com>
2021-10-28 20:01:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+    let matches = drivers.filter(driver => 
+        driver.toLowerCase() == name.toLowerCase()
+    )
+    return matches
+}
+
+function fuzzyMatch(drivers, testString) {
+    return drivers.filter( driver =>
+      driver.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter( record => record.name === name)
+  }
